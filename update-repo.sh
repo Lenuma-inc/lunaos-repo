a325fcbc9f14790b5dc5c3e409122bd1c69216b9
Boria138 <boriabloger@protonmail.com>
2023-07-26 13:12:32 +0600

    Update repo

--- update-repo.sh
+++ update-repo.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 for pkg in *.pkg.tar.zst; do
   echo "Adding package $pkg to repository"
-  repo-add -n -R $repo.db.tar.gz $pkg
-  rm $repo.db
-  rm $repo.files
-  mv $repo.db.tar.gz $repo.db
-  mv $repo.files.tar.gz $repo.files
+  repo-add $repo.db.tar.gz $pkg
 done

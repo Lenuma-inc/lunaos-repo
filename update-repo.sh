b5b3a875509421083bc5eb05e0f6986551d46982
Boria138 <boriabloger@protonmail.com>
2023-07-26 14:07:48 +0600

    Clean unused files

--- update-repo.sh
+++ update-repo.sh
@@ -2,4 +2,8 @@
 for pkg in *.pkg.tar.zst; do
   echo "Adding package $pkg to repository"
   repo-add $repo.db.tar.gz $pkg
+  rm $repo.db
+  rm rm $repo.files
+  mv $repo.db.tar.gz $repo.db
+  mv $repo.files.tar.gz $repo.files
 done

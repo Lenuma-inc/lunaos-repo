b87b65537d36ecdc9766e282c834a5b58c5ca84d
Boria138 <boriabloger@protonmail.com>
2023-07-26 09:08:05 +0600

    Fix typo

--- update-repo.sh
+++ update-repo.sh
@@ -4,6 +4,6 @@ for pkg in .pkg.tar{.xz,.zst}; do
   repo-add -n -R  lunaos-repo.db.tar.gz $pkg
   rm lunaos-repo.db
   rm lunaos-repo.files
-  mv lunaos-repo.db.tar.gz dtos-core-repo.db
-  mv lunaos-repo.files.tar.gz dtos-core-repo.files
+  mv lunaos-repo.db.tar.gz lunaos-repo.db
+  mv lunaos-repo.files.tar.gz lunaos-repo.files
 done

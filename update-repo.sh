18ef6935ec92bb5f81471d3e3692da193013a480
Boria138 <boriabloger@protonmail.com>
2023-07-26 10:35:10 +0600

    Revent update repo

--- update-repo.sh
+++ update-repo.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-for pkg in .pkg.tar{.xz,.zst}; do
+for pkg in *.pkg.tar.zst; do
   echo "Adding package $pkg to repository"
-  repo-add -n -R  lunaos-repo.db.tar.gz $pkg
-  rm lunaos-repo.db
-  rm lunaos-repo.files
-  mv lunaos-repo.db.tar.gz lunaos-repo.db
-  mv lunaos-repo.files.tar.gz lunaos-repo.files
+  repo-add --sign --verify $repo.db.tar.gz $pkg
 done

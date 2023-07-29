0c744e92239b5db87f5b4730fb45b36d845f4e08
Boria138 <boriabloger@protonmail.com>
2023-07-29 13:49:24 +0600

    Update repo

--- update-repo.sh
+++ update-repo.sh
@@ -2,9 +2,4 @@
 for pkg in *.pkg.tar.zst; do
   echo "Adding package $pkg to repository"
   repo-add $repo.db.tar.gz $pkg
-  echo "Clearing symbolic link from the $repo"
-  rm $repo.db
-  rm $repo.files
-  mv $repo.db.tar.gz $repo.db
-  mv $repo.files.tar.gz $repo.files
 done

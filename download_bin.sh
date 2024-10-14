aeafee811eacecef838076e29e4461fcdbcd1bdf
Boris Yumankulov <boria138@altlinux.org>
2024-10-14 15:40:08 +0500

    Replace HHD kernel with chimeraos

--- download_bin.sh
+++ download_bin.sh
@@ -2,8 +2,33 @@
 
 LINUX_G14_VER=$(curl -s https://arch.asus-linux.org/ | grep -oP "linux-g14-\d+\.\d+\.\d+\.arch1-1(?:\.\d+)?-x86_64\.pkg\.tar\.zst" | sort -V | tail -1)
 HEADERS_G14_VER=$(curl -s https://arch.asus-linux.org/ | grep -oP "linux-g14-headers-\d+\.\d+\.\d+\.arch1-1(?:\.\d+)?-x86_64\.pkg\.tar\.zst" | sort -V | tail -1)
-LINUX_HANDHELD_URL=$(curl -s "https://api.github.com/repos/hhd-dev/linux-handheld/releases/latest" | grep "browser_download_url" | cut -d '"' -f 4)
+LINUX_CHOS_URL=$(curl -s "https://api.github.com/repos/ChimeraOS/linux-chimeraos/releases" \
+        | awk '{
+    if ($0 ~ /"prerelease": false/) {
+        prerelease = 0;
+    } else if ($0 ~ /"prerelease": true/) {
+        prerelease = 1;
+    }
+    if (!prerelease && $0 ~ /"browser_download_url":/) {
+        match($0, /"browser_download_url": "(https[^"]*)"/, arr);
+        print arr[1];
+    }
+}' | grep -i "linux-chimeraos" | head -1)
+LINUX_CHOS_HEADERS_URL=$(curl -s "https://api.github.com/repos/ChimeraOS/linux-chimeraos/releases" \
+        | awk '{
+    if ($0 ~ /"prerelease": false/) {
+        prerelease = 0;
+    } else if ($0 ~ /"prerelease": true/) {
+        prerelease = 1;
+    }
+    if (!prerelease && $0 ~ /"browser_download_url":/) {
+        match($0, /"browser_download_url": "(https[^"]*)"/, arr);
+        print arr[1];
+    }
+}' | grep "linux-chimeraos-" | head -1)
 
 wget "https://arch.asus-linux.org/$LINUX_G14_VER"
 wget "https://arch.asus-linux.org/$HEADERS_G14_VER"
-wget $LINUX_HANDHELD_URL
+wget $LINUX_CHOS_URL
+wget $LINUX_CHOS_HEADERS_URL
+
